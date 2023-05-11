3fac9a12d7bbe9ec49be2c348b3e68c965aec83d
Ferorg <123727750+Ferorg@users.noreply.github.com>
2023-05-11 14:09:12 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,24 @@
+import java.io.Serializable;
+
+public class Car implements Serializable {
+    private String name;
+    private  int probeg;
+    public Car(String name, int probeg) {
+        this.name = name;
+        this.probeg = probeg;
+    }
+    public void go(){
+        for(int i=0;i<10;i++){
+            probeg+=10;
+            System.out.println(name+" проехал 10км");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", probeg=" + probeg +
+                '}';
+    }
+}
